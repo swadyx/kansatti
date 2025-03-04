104f6d196068adb4c358248d8acc8c1c75df8c92
hasorez <127985285+hasorez@users.noreply.github.com>
2025-03-04 07:28:30 +0200

    Update definitions.h

---
+++ FlightCanSatCode/definitions.h
@@ -0,0 +1,30 @@
+#ifndef DEFINITIONS_H 
+#define DEFINITIONS_H
+
+TinyGPSPlus gps;
+
+struct MQSensorData {
+  int mq135;
+  int mq4;
+};
+
+struct SCD40Data {
+  uint16_t co2Concentration;
+  float temperature;
+  float relativeHumidity;
+  int error; // error code: NO_ERROR (0) means success; non-zero indicates an error
+};
+
+struct GPSData {
+  bool dataUpdated;     // Indicates if new GPS data is available
+  double latitude;
+  double longitude;
+  double altitude;
+  double speed;
+  uint8_t satellites;
+  int hour;
+  int minute;
+  int second;
+};
+
+#endif // DEFINITIONS_H
